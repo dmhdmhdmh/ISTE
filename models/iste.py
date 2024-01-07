9382cc53324beac9b3f0d846a73d9b67c5e353bd
Minghong Duan <56492295+dmhdmhdmh@users.noreply.github.com>
2024-01-07 14:59:04 +0800

    Update iste.py

--- models/iste.py
+++ models/iste.py
@@ -18,10 +18,8 @@ class ISTE(nn.Module):
                  hidden_dim=256):
         super().__init__()
         self.encoder = models.make(encoder_spec)
-        # self.coef = nn.Conv2d(self.encoder.out_dim, hidden_dim, 3, padding=1)
-        # self.freq = nn.Conv2d(self.encoder.out_dim, hidden_dim*2, 3, padding=1)
-        # self.phase = nn.Linear(2, hidden_dim//2, bias=False)
         self.liifconv = nn.Conv2d(self.encoder.out_dim, 256, 3, padding=1)
+        # Texture Learner各个组件
         self.coef = nn.Conv2d(self.encoder.out_dim, 256, 3, padding=1)
         self.freq1 = nn.Conv2d(self.encoder.out_dim, 256, 3, padding=1)
         self.freq2 = nn.Conv2d(self.encoder.out_dim, 256, 3, padding=1)
@@ -31,25 +29,29 @@ class ISTE(nn.Module):
         self.local_ensemble = local_ensemble
         self.cell_decode = cell_decode
         self.m = 0.999
+        # Local Texture Decoder
         self.imnet_texture = models.make(imnet_spec, args={'in_dim': 256})
         self.imnet_texture_auxiliary = models.make(imnet_spec, args={'in_dim': 256})
+        # Local Pixel Decoder
         self.imnet_rgb = mlp.MLP(in_dim=260, out_dim=3, hidden_list=[256, 256, 256])
         self.imnet_query = mlp.MLP(in_dim=3, out_dim=256, hidden_list=[256, 256, 256])
+        # Local Feature Interactor模块
         self.pix_attn = pixattn.pix_attn(is_pooling=True)
         self.fusionlinear = nn.Linear(512, 256, bias=False)
-        #self.lte_query = nn.Linear(2, hidden_dim)
-        #self.lte_value = nn.Linear(64, hidden_dim)
-        #self.lte_key = nn.Linear(64, hidden_dim)
 
     def gen_feat(self, inp):
         self.inp = inp
+        #低分辨率输入图像坐标
         self.feat_coord = make_coord(inp.shape[-2:], flatten=False).cuda() \
             .permute(2, 0, 1) \
             .unsqueeze(0).expand(inp.shape[0], 2, *inp.shape[-2:])
-
+        #特征预提取F_LR
         self.feat = self.encoder(inp)
+        #Texture Learner模块中上采样前特征F_Amp
         self.coeff = self.coef(self.feat)
+        #Texture Learner模块中上采样前特征F_FreqX
         self.freqq1 = self.freq1(self.feat)
+        #Texture Learner模块中上采样前特征F_FreqY
         self.freqq2 = self.freq2(self.feat)
         return self.feat
 
@@ -125,7 +127,8 @@ class ISTE(nn.Module):
         for pred, area in zip(preds, areas):
             ret = ret + pred * (area / tot_area).unsqueeze(-1)
         return ret
-
+        
+    # STF模块中的纹理检索操作
     def bis(self, input, dim, index):
         views = [input.size(0)] + [1 if i!=dim else -1 for i in range(1, len(input.size()))]
         expanse = list(input.size())
@@ -138,6 +141,7 @@ class ISTE(nn.Module):
         feat = self.feat
         # coef = self.coeff
         # freq = self.freqq
+        # Texture Learner模块中上采样前特征F_Amp，F_FreqX，F_FreqY
         coef = self.coeff
         freq1 = self.freqq1
         freq2 = self.freqq2
@@ -153,10 +157,9 @@ class ISTE(nn.Module):
             feat_liif = F.unfold(feat, 3, padding=1).view(
                 feat.shape[0], feat.shape[1] * 9, feat.shape[2], feat.shape[3])
         '''
-
+        # Local Feature Interactor模块调用输出F_LFI
         feat = self.pix_attn(self.feat)
-        #feat_liif = self.feat
-
+        # 输出F_LFIC
         feat_liif = self.liifconv(feat)
 
         vx_lst = [-1, 1]
@@ -167,7 +170,7 @@ class ISTE(nn.Module):
         rx = 2 / feat.shape[-2] / 2
         ry = 2 / feat.shape[-1] / 2
 
-        # feat_cell_s = torch.tensor(4)
+        # F_LR坐标
         feat_coord = self.feat_coord
 
         preds_texture = []
@@ -188,37 +191,27 @@ class ISTE(nn.Module):
                     feat_liif, coord_.flip(-1).unsqueeze(1),
                     mode='nearest', align_corners=False)[:, :, 0, :] \
                     .permute(0, 2, 1)
-
+                #Texture Learner中特征向量Amp
                 q_coef = F.grid_sample(
                     coef, coord_.flip(-1).unsqueeze(1),
                     mode='nearest', align_corners=False)[:, :, 0, :] \
                     .permute(0, 2, 1)
-
-                '''
-                lte_feat = F.grid_sample(
-                    feat, coord_.flip(-1).unsqueeze(1),
-                    mode='nearest', align_corners=False)[:, :, 0, :] \
-                    .permute(0, 2, 1)
-
-                q_freq = F.grid_sample(
-                    freq, coord_.flip(-1).unsqueeze(1),
-                    mode='nearest', align_corners=False)[:, :, 0, :] \
-                    .permute(0, 2, 1)
-                '''
-
+                #Texture Learner中特征向量FreqX
                 q_freq1 = F.grid_sample(
                     freq1, coord_.flip(-1).unsqueeze(1),
                     mode='nearest', align_corners=False)[:, :, 0, :] \
                     .permute(0, 2, 1)
+                #Texture Learner中特征向量FreqY
                 q_freq2 = F.grid_sample(
                     freq2, coord_.flip(-1).unsqueeze(1),
                     mode='nearest', align_corners=False)[:, :, 0, :] \
                     .permute(0, 2, 1)
-
+                #HR图像中与LR图像坐标最近的坐标(X',Y')
                 q_coord = F.grid_sample(
                     feat_coord, coord_.flip(-1).unsqueeze(1),
                     mode='nearest', align_corners=False)[:, :, 0, :] \
                     .permute(0, 2, 1)
+                #Local Grid (X'-X,Y'-Y)
                 rel_coord = coord - q_coord
                 # rel_coord2 =  coord - q_coord
                 rel_coord[:, :, 0] *= feat.shape[-2]
@@ -232,61 +225,37 @@ class ISTE(nn.Module):
                 rel_cell[:, :, 1] *= feat.shape[-1]
 
 
-                # basis generation
-
-                '''
-                q_freq = torch.stack(torch.split(q_freq, 2, dim=-1), dim=-1)
-                q_freq = torch.mul(q_freq, rel_coord.unsqueeze(-1))
-                q_freq = torch.sum(q_freq, dim=-2)
-                phase = 2 * np.pi * (rel_coord_s/feat_cell_s).unsqueeze(-1)
-                #* torch.ones(bs,q).cuda()
-                q_freq = torch.sin(np.pi*q_freq + phase)
-                '''
-
-                # q_freq += self.phase(rel_cell.view((bs * q, -1))).view(bs, q, -1)
-
-                # q_freq = torch.cat((torch.cos(np.pi*q_freq), torch.sin(np.pi*q_freq)), dim=-1)
-                '''
-                lte_query = self.lte_query(rel_coord)
-                lte_value = self.lte_value(lte_feat)
-                lte_key = self.lte_key(lte_feat)
-                lte_phase = self.phase(rel_cell)
-
-                inp_texture = lte_value * torch.sin(lte_query * lte_key + lte_phase)
-                '''
-
+                ## 纹理特征F_TL的计算
                 q_freq = torch.stack((q_freq1, q_freq2), dim=-2)
                 q_freq = torch.mul(q_freq, rel_coord.unsqueeze(-1))
                 q_freq = torch.sum(q_freq, dim=-2)
                 phase = self.phase(rel_cell.view((bs * q, -1))).view(bs, q, -1)
                 phase = 2 * np.pi * self.toone(phase)
-                # * torch.ones(bs,q).cuda()
                 q_freq = torch.sin(q_freq + phase)
 
                 inp_texture = torch.mul(q_coef, q_freq)
 
                 # K_unfold = K_unfold.permute(0, 2, 1)
 
-
+                ## Self-Texture Fusion模块中Q，K，V的构建
                 K = inp_texture
                 Q = q_feat_liif_key.permute(0, 2, 1)
                 V = inp_texture.permute(0, 2, 1)
 
                 KF = F.normalize(K, dim=2)  # [N, Hr*Wr, C*k*k]
                 QF = F.normalize(Q, dim=1)  # [N, C*k*k, H*W]
-
+                # Q和K相似度计算，得到软注意力图R_lv3_star和纹理特征F_TL中与像素特征图F_LFIC中每个特征向量最相似的纹理特征向量位置
                 R_lv3 = torch.bmm(KF, QF)  # [N, Hr*Wr, H*W]
                 R_lv3_star, R_lv3_star_arg = torch.max(R_lv3, dim=1)  # [N, H*W]
-
+                # Texture Selection，将与像素特征图F_LFIC中每个特征向量最相似的纹理特征向量从F_TL中挑选出来
                 T_lv3 = self.bis(V, 2, R_lv3_star_arg)
-
+                # 将像素特征图F_LFIC和挑选出的纹理特征级联并通过线性映射后和软注意力图相乘，最后和残差连接和像素特征图F_LFIC相加得到输出第一阶段纹理增强特征F_STF，即代码中的inp_feat
                 T = torch.cat([Q, T_lv3], 1)
                 T = T.permute(0, 2, 1)
                 T = self.fusionlinear(T)
                 inp_feat = q_feat_liif_key + T * R_lv3_star.unsqueeze(-1)
 
-                # pred_inp = inp.contiguous().view(bs * q, -1)
-
+                # 将像素特征图F_LFIC通过基于隐式神经表达的Local Pixel Decoder解码到空域
                 inp_liif = torch.cat([inp_feat, rel_coord], dim=-1)
 
                 if self.cell_decode:
@@ -295,19 +264,10 @@ class ISTE(nn.Module):
                     rel_cell[:, :, 1] *= feat.shape[-1]
                     inp_liif = torch.cat([inp_liif, rel_cell], dim=-1)
 
-
-
                 pred_rgb = self.imnet_rgb(inp_liif.view(bs * q, -1)).view(bs, q, -1)
                 preds_rgb.append(pred_rgb)
-
-
-
-                '''
-                attention_scores = torch.sum(Query * Key, dim=-1) / math.sqrt(Key.shape[-1])
-                attention_scores = nn.Softmax(dim=-1)(attention_scores)
-                inp_texture = attention_scores.unsqueeze(-1) * Value
-                '''
-
+                
+                # 将纹理特征图F_TL通过基于隐式神经表达的Local Texture Decoder解码到空域
                 pred_texture = self.imnet_texture(inp_texture.contiguous().view(bs * q, -1)).view(bs, q, -1)
 
                 preds_texture.append(pred_texture)
@@ -321,13 +281,14 @@ class ISTE(nn.Module):
 
         ret_texture = 0
         ret_rgb = 0
-
+        # Local Ensemble
         for pred_texture, area in zip(preds_texture, areas):
             ret_texture = ret_texture + pred_texture * (area / tot_area).unsqueeze(-1)
 
         for pred_rgb, area in zip(preds_rgb, areas):
             ret_rgb = ret_rgb + pred_rgb * (area / tot_area).unsqueeze(-1)
 
+        #将纹理特征F_TL解码结果和第一次纹理增强特征F_STF解码结果相加实现空域纹理增强
         output = ret_texture + ret_rgb
         '''
         ret += F.grid_sample(self.inp, coord.flip(-1).unsqueeze(1), mode='bilinear',\
